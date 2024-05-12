f4f930cd1035c320f4d830609032ace2e89b8be2
Noelia <sharimanoe@gmail.com>
2024-05-12 15:25:06 +0200

    lab solution

--- src/pages/AddBeerPage.jsx
+++ src/pages/AddBeerPage.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+const apiURL = "https://ih-beers-api2.herokuapp.com/beers/new";
 
 function AddBeerPage() {
+  const navigate = useNavigate();
   // State variables to store the values of the form inputs. You can leave these as they are.
   const [name, setName] = useState("");
   const [tagline, setTagline] = useState("");
@@ -21,20 +25,56 @@ function AddBeerPage() {
   const handleAttenuationLevel = (e) => setAttenuationLevel(e.target.value);
   const handleContributedBy = (e) => setContributedBy(e.target.value);
 
-
-
   // TASK:
   // 1. Create a function to handle the form submission and send the form data to the Beers API to create a new beer.
   // 2. Use axios to make a POST request to the Beers API.
   // 3. Once the beer is created, navigate the user to the page showing the list of all beers.
 
+  const handleSubmit = (e) => {
+    // Prevent form from refreshing page
+    e.preventDefault();
+    // Create new product object
+    const newBeer = {
+      name: name,
+      tagline: tagline,
+      description: description,
+      image_url: imageUrl,
+      first_brewed: firstBrewed,
+      brewers_tips: brewersTips,
+      attenuation_level: attenuationLevel,
+      contributed_by: contributedBy,
+    };
 
+    axios
+      .post(apiURL, newBeer)
+      .then((response) => {
+        console.log("Beer added successfully:", response.data);
+        // Optionally, navigate to another page after adding the beer
+        // Example: history.push("/beers");
+      })
+      .catch((error) => {
+        console.error("Error adding beer:", error);
+      });
+
+    // Reset form fields
+    setName("");
+    setTagline("");
+    setDescription("");
+    setImageUrl("");
+    setFirstBrewed("");
+    setBrewersTips("");
+    setAttenuationLevel(0);
+    setContributedBy("");
+
+    //been redirect to home page
+    navigate("/");
+  };
 
   // Structure and the content of the page showing the form for adding a new beer. You can leave this as it is.
   return (
     <>
       <div className="d-inline-flex flex-column w-100 p-4">
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>Name</label>
           <input
             className="form-control mb-4"
